40db1313beb170d006f63a4381a1bf1570ff9bb8
Jonah Pierce <fellowhashbrown@gmail.com>
2020-10-28 23:09:12 +0000

    Added example functions to main.py

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ f = QM("ABC", [4, 5, 6, 7])
 print(f.solve()) # --> A
  
 g = QM("ABC", [3, 4, 5, 6, 7])
-print(g.solve()) # --> A OR B AND C
+print(g.solve()) # --> A + B*C
 
-# # # # # 
+# # # # #
 
 f = QM("ABC", [0, 1, 2, 3], is_maxterm = True)
-print(f.solve()) # --> NOT A
-
+print(f.solve()) # --> A'
+ 
 g = QM("ABC", [0, 1, 2], is_maxterm = True)
-print(g.solve()) # --> (NOT A OR NOT B) AND (NOT A OR NOT C)
+print(g.solve()) # --> (A'+B')*(A'+C')
 
 # # # # #
 
 f = QM("ABC", [0, 1], dont_cares = [4, 5, 6, 7])
-print(f.solve()) # --> NOT B
-
+print(f.solve()) # --> B'
+ 
 g = QM("ABCD", [0, 2, 4, 8, 12], dont_cares = [6, 10, 11, 14, 15])
-print(g.solve()) # --> NOT D
+print(g.solve()) # --> D'
