c3f7bc4a494c1478563942cc82e681bf53990792
Jonah Pierce <fellowhashbrown@gmail.com>
2020-10-28 21:52:12 +0000

    Updated module doc in QM.py

--- qm.py
+++ qm.py
@@ -1,5 +1,5 @@
 """
-Copyright 2020 Jonah Pierce
+Copyright (c) 2019 Jonah Pierce
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
